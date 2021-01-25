fc001d9ddc3c203ab0d12f3ee79e7622fa5b6dc0
Guy Vider <guy@guyvider.com>
2021-01-24 16:40:57 -0800

    deploy using env values

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,15 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-
+require('dotenv').config();
 require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    rinkeby: {
+      url: process.env.INFURA_RINKBEY_URL,
+      accounts: [`0x${process.env.RINKBEY_PRIVATE_KEY}`]
+    }
+  }
 };
